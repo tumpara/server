5e7d017330f083c874fbdbafb82588c1e269ae30
Yannik Rödel <hey@yannik.info>
2021-07-09 12:49:41 +0200

    Hide output for group management commands

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 for gid in $TUMPARA_EXTRA_GROUPS; do
-  groupadd "$gid" -g "$gid" >/dev/null
-  gpasswd -a tumpara "$gid" >/dev/null
+  groupadd "$gid" -g "$gid" >/dev/null 2>&1
+  gpasswd -a tumpara "$gid" >/dev/null 2>&1
 done
 
 # Explicitly leaving out the quotes around '$*' so we can support commands with
