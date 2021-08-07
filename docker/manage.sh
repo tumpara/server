039a9fa5864c9a15f95dddd59f0ac85fcec5ee21
Yannik Rödel <hey@yannik.info>
2021-08-07 13:52:27 +0200

    Refactor Docker image to an ubuntu base
    It should now also work with all the dependencies we need.

--- docker/manage.sh
+++ docker/manage.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # As in entrypoint.sh, we leave out the quotes around '$*' here.
-/entrypoint.sh python -m tumpara $*
+/opt/tumpara/entrypoint.sh python3 -m tumpara $*
--- docker/tumpara.sh
+++ docker/tumpara.sh
@@ -5,9 +5,9 @@ normal=$(tput sgr0)
 
 echo ""
 echo "${bold}[Tumpara]${normal} Running pre-startup actions..."
-python -m tumpara migrate --noinput
-python -m tumpara collectstatic -c --noinput
+python3 -m tumpara migrate --noinput
+python3 -m tumpara collectstatic -c --noinput
 
 echo ""
 echo "${bold}[Tumpara]${normal} Starting server processes..."
-gunicorn --bind 0.0.0.0:80 tumpara.wsgi
+python3 -m gunicorn --bind 0.0.0.0:80 tumpara.wsgi
